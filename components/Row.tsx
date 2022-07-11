2003ce2ba6d94fb2f8cc6714242a20df7aaa9f2b
Anshu Verma <verma.anshu96@gmail.com>
2022-07-11 21:08:22 +0530

    Added thumbnail

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,5 +1,7 @@
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
 import React from 'react'
 import { Movie } from '../types'
+import Thumbnail from './Thumbnail'
 
 interface Props {
     title: string,
@@ -8,7 +10,28 @@ interface Props {
 
 const Row = ({ title, movies }: Props) => {
     return (
-        <div>{title}</div>
+        <div className='h-40 space-y-0.5 md:space-y-2'>
+            <h2 className='w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition
+             duration-200 hover:text-white md:text-2xl'>
+                {title}
+            </h2>
+            <div className='group relative md:-ml-2'>
+                <ChevronLeftIcon className='absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-default opacity-0
+                transition hover:scale-125 group-hover:opacity-100' />
+
+                <div className='flex scrollbar-hide items-center space-x-1 overflow-x-scroll md:space-x-2.5
+                md:p-2'>
+                    {movies.map((movie)=>{
+                        return (
+                            <Thumbnail key={movie.id} movie={movie}/>
+                        )
+                    })}
+                </div>
+
+                <ChevronRightIcon className='absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-default opacity-0
+                transition hover:scale-125 group-hover:opacity-100' />
+            </div>
+        </div>
     )
 }
 
